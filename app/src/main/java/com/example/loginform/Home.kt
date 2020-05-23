43f48a83e30b12e10c9dc0726874515f562e3a80
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-05-23 21:48:27 +0530

    firstcommit

---
+++ app/src/main/java/com/example/loginform/Home.kt
@@ -0,0 +1,12 @@
+package com.example.loginform
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// IT18233704 N.R Yamasinghe
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
